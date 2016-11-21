3da58930908920f65c6651289db9d190002cd667
trevortds <trevortds3@gmail.com>
2016-11-21 12:18:31 -0700

    fixed regex categorizer, now gets pretty close. Added statistical categorizer.

--- regex_categorizer.py
+++ regex_categorizer.py
@@ -1,38 +1,129 @@
 import tsvopener
+import random
+import re
 
 etymdict = tsvopener.etymdict
 
+danger = False
 
-category_dict = {}
+category_dict = {"buzz (n.)": "Other"}
 
 hierarchies = {
     "Greek": ["Greek"],
     "Latin": ["Latin", "Etruscan"],
     "Norse": ["Old Norse", "Proto-North-Germanic", "North Germanic",
-              ],
+              "Scandanavian", "Danish"],
     "French": ["French", "Middle French", "Old French", "Medieval Latin",
                ],
     "English": ["Old English", "Ingvaeonic", "Frisian", "Saxon",
                 "Proto-West-Germanic", "Proto-Germanic", "Germanic"],
 }
 
+order = ["Greek", "Latin", "Norse", "French", "English"]
 
-def regex_categorize(definition):
+from_pattern = re.compile("[Ff]rom (\w+ \(\w+.\d?\)|\w*-)")
+from_paren_optional_pattern = re.compile("[Ff]rom (\w\w+( \(\w+.\d?\))?|\w*-)")
+see_pattern = re.compile("[sS]ee (\w+ \(\w+.\d?\))")
+see_paren_optional_pattern = re.compile("[Ss]ee (\w+( \(\w+.\d?\))?)")
+variant_pattern = re.compile("[Vv]ariant of (\w+ \(\w+.\d?\))")
+
+
+def regex_categorize(word, definition, depth=0):
     '''
     Steps through the above hierarchies to find which categorization is
         appropriate based on what words are present. The older languages come
         first because they may be overwritten later.
 
     '''
+    # making this recursive, so we need a base case
+    if word in category_dict.keys():
+        return category_dict[word]
+    if depth >= 10:
+        return "ERR"
+
     output = ""
-    for category, chain in hierarchies.items():
+    for category in order:
+        chain = hierarchies[category]
         for item in chain:
             if item in definition:
                 output = category
 
     if output == "":
-        output = "Other"
+        # there's no language mentioned: doesn't mean other automaticall
+        # could be a compound
+        # check if there's a from x
+        done = False
+        if from_pattern.search(definition) is not None:
+            # check if x is in dictionary, if so, use the category of x
+            tst = from_pattern.search(definition).group(1)
+            if tst in etymdict.keys():
+                output = regex_categorize(tst, etymdict[tst], depth+1)
+            if output != "ERR":
+                done = True
+        # check if there's a see x
+        if not done and see_pattern.search(definition) is not None:
+            # check is x is in dictionary, if so, use the category of x
+            tst = see_pattern.search(definition).group(1)
+            if tst in etymdict.keys():
+                output = regex_categorize(tst, etymdict[tst], depth+1)
+            if output != "ERR":
+                done = True
+        # variant of x
+        if not done and variant_pattern.search(definition) is not None:
+            tst = variant_pattern.search(definition).group(1)
+            if tst in etymdict.keys():
+                output = regex_categorize(tst, etymdict[tst], depth+1)
+            if output != "ERR":
+                done = True
+        # danger zone: these patterns will match non-etymological connections,
+        #     like "from portuguese"
+        if (not done and danger and
+                (from_paren_optional_pattern.search(definition) is not None)):
+            tst = from_paren_optional_pattern.search(definition).group(1)
+            if tst in etymdict.keys():
+                output = regex_categorize(tst, etymdict[tst], depth+1)
+            if output != "ERR":
+                done = True
+        if (not done and danger and
+                (see_paren_optional_pattern.search(definition) is not None)):
+            tst = see_paren_optional_pattern.search(definition).group(1)
+            if tst in etymdict.keys():
+                output = regex_categorize(tst, etymdict[tst], depth+1)
+            if output != "ERR":
+                done = True
+        if not done:
+            # else, other
+            output = "Other"
     return output
+
+
+def manually_check(etymdict, category_dict, num):
+    '''
+    takes judgements from the user, returns number right, number wrong,
+        number total
+    '''
+    wordlist = list(etymdict.keys())
+    random.shuffle(wordlist)
+    # shuffle and then truncate the list
+    wordlist = wordlist[:num]
+    numright = 0
+    numwrong = 0
+    numtotal = 0
+    for word in wordlist:
+        print("Word: ", word, " Category: ", category_dict[word])
+        print("Definition: ", etymdict[word])
+        numtotal += 1
+        while True:
+            judge = input("\nCorrect? [enter for yes, n for no, c to close]:")
+            if judge == '':
+                numright += 1
+            elif judge == 'n':
+                numwrong += 1
+            elif judge == 'c':
+                return numright, numwrong, numtotal
+            else:
+                continue
+            break
 
 
 if __name__ == '__main__':
@@ -47,7 +138,7 @@ if __name__ == '__main__':
     print("categorizing")
 
     for word, definition in etymdict.items():
-        cat = regex_categorize(definition)
+        cat = regex_categorize(word, definition)
         category_dict[word] = cat
         if cat == "English":
             eng += 1
@@ -73,3 +164,5 @@ if __name__ == '__main__':
     print("Other: ", other, " percent of total: ", other / tot * 100)
 
     print("Total: ", tot)
+
+    tsvopener.writeitout(category_dict, "categorized.tsv")
---
+++ statistical_categorizer.py
@@ -0,0 +1,90 @@
+import tsvopener
+import csv
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+from sklearn import svm
+
+# set these to true and run whenever categorized.tsv changes
+initletters = False
+initwords = False
+
+stops = stopwords.words('english')
+etymdict = tsvopener.etymdict
+category_dict = {}
+
+with open('categorized.tsv', 'r') as readfile:
+    etymreader = csv.reader(readfile, delimiter='\t', quotechar='|')
+
+    for line in etymreader:
+        category_dict[line[0]] = line[1]
+
+
+allwords = []
+allletters = []
+
+if initwords:
+    allwords = set()
+    # start off as sets to prevent duplicates
+    for item in etymdict.keys():
+        for word in word_tokenize(etymdict[item]):
+            if word in stops:
+                continue
+            else:
+                allwords.add(word)
+
+    allwords = list(allwords)
+    with open("words.txt", 'w') as writefile:
+        for word in allwords:
+            writefile.write(word + "\n")
+else:
+    with open("words.txt", "r") as readfile:
+        for line in readfile:
+            allwords.append(line)
+
+if initletters:
+    allletters = set()
+    for word in allwords:
+        for letter in word:
+            allletters.add(letter)
+
+    allletters = list(allletters)
+    with open("letters.txt", 'w') as writefile:
+        for letter in allletters:
+            writefile.write(letter + "\n")
+else:
+    with open("letters.txt", 'r') as readfile:
+        for line in readfile:
+            allletters.append(line)
+
+
+def bag_of_words_featurizer(word, definition):
+    output = [0] * len(allwords)
+    # initialize output vector
+    for definition_word in word_tokenize(definition):
+        if definition_word not in stops:
+            output[allwords.index(definition_word)] += 1
+
+    return output
+
+
+X = []
+t = []
+
+for key in category_dict.keys():
+    X.append(bag_of_words_featurizer(key, etymdict[key]))
+    if category_dict[key] == "English":
+        t.append(0)
+    elif category_dict[key] == "French":
+        t.append(1)
+    elif category_dict[key] == "Norse":
+        t.append(2)
+    elif category_dict[key] == "Latin":
+        t.append(3)
+    elif category_dict[key] == "Greek":
+        t.append(4)
+    elif category_dict[key] == "Other":
+        t.append(5)
+    else:
+        t.append(5)
+
+
